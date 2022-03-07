e3179fe1c162ef8e5c250c05b3d7dbf68498acf7
GS\guitc <guilherme.tcavalcanti@gmail.com>
2022-03-06 22:54:19 -0300

    kRPC arduino testing
    making rpc calls and getting altitude reading to show on lcd display

--- Source/Code/Arduino/kRPC/kRPC--LCD16X2/kRPC--LCD16X2.ino
+++ Source/Code/Arduino/kRPC/kRPC--LCD16X2/kRPC--LCD16X2.ino
@@ -1,11 +1,10 @@
 /*
  *  This test code uses one LCD Display and a joystick, to control the display
- *  
  */
-
 
 #include <krpc.h>
 #include <krpc/services/krpc.h>
+#include <krpc/services/space_center.h>
 
 #include <LiquidCrystal.h>
 
@@ -19,6 +18,11 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 #define Y_PIN A0
 #define X_PIN A1
 
+krpc_SpaceCenter_Vessel_t vessel;
+krpc_SpaceCenter_Flight_t flight;
+
+double vessel_altitude;
+
 void setup() {
   
   conn = &Serial;
@@ -29,10 +33,17 @@ void setup() {
 
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
 
-  lcd_print_all_page();
+  krpc_SpaceCenter_ActiveVessel(conn, &vessel);
+
+  krpc_SpaceCenter_Vessel_set_Name(conn, vessel, "My Vessel");
+
+  // Get a handle to a Flight object for the vessel
+  krpc_SpaceCenter_Vessel_Flight(conn, &flight, vessel, KRPC_NULL);
+  // Get the altiude
+  krpc_SpaceCenter_Flight_MeanAltitude(conn, &vessel_altitude, flight);
+
+  //lcd_print_all_page();
 
   pinMode(Y_PIN, INPUT);
   pinMode(X_PIN, INPUT);
@@ -50,24 +61,39 @@ void loop() {
 
   int relative_x = analogRead(X_PIN) - last_x_read;
   int relative_y = analogRead(Y_PIN) - last_y_read;
+
+  bool right = relative_x < 0;
+
+  //lcd_scroll(right, abs(relative_x));
+
+  krpc_SpaceCenter_Flight_MeanAltitude(conn, &vessel_altitude, flight);
+
+  lcd.setCursor(1,0);
+  lcd.print(vessel_altitude);
   
-  if (analogRead(X_PIN) > JOYSTICK_CHECK_HIGH_THRESHOLD && millis() - last_mill >  _update_delay)
+}
+
+void lcd_scroll(bool right, int scroll_speed){
+  if (right)
   {
-    lcd.scrollDisplayLeft();
-    last_mill = millis();
+    for (int i = 0; i < scroll_speed; i ++)
+    {
+      lcd.scrollDisplayRight();
+    }
   }
-  
-  if (analogRead(X_PIN) < JOYSTICK_CHECK_LOW_THRESHOLD && millis() - last_mill > _update_delay)
+  else
   {
-    lcd.scrollDisplayRight();
-    last_mill = millis();
+    for (int i = 0; i < scroll_speed; i ++)
+    {
+      lcd.scrollDisplayLeft();
+    }
   }
 }
 
 void lcd_print_all_page(){
   // print all static screen interface
-  lcd.setCursor(1, 2);
+  lcd.setCursor(0, 1);
   lcd.print("Kontroller - Kerbal Space Program");
-  lcd.setCursor(40, 2);
+  lcd.setCursor(40, 1);
   lcd.print("this works!");
 }
